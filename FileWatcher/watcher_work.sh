f003ad5c905b30b5eeb708256ec30faf6189b1ed
muslih_k_diy <69723789+muslih-DIY@users.noreply.github.com>
2022-08-24 16:03:12 +0530

    Delete FileWatcher directory

--- FileWatcher/watcher_work.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-DIR=$(dirname "${BASH_SOURCE[0]}")
-echo "changeon $1:$2" >> $DIR/change_detecion.log
--- FileWatcher/watchman.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-WATCHDIR=/Myproject/
-DIR=$(dirname "${BASH_SOURCE[0]}")
-inotifywait -q -m -r -e modify,move,delete,create $WATCHDIR | while read DIRECTORY EVENT FILE; do
-bash $DIR/watcher_process.sh $EVENT $FILE
-done
